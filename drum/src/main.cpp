ad3a38edf1f4e89a5104788853db7fba405a8987
Harsh Renose <renoseharsh@gmail.com>
2026-07-15 08:56:32 +0530

    main: use overloaded visitor for command dispatch

--- drum/src/main.cpp
+++ drum/src/main.cpp
@@ -2,9 +2,22 @@ import std;
 import parser;
 import executor;
 
+template <class... Ts> struct overloaded : Ts... {
+  using Ts::operator()...;
+};
+template <class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
 int main(int argc, char *argv[]) {
   if (const auto result = drum::parser::parse_arguments(argc, argv); result) {
-    auto res = std::visit(drum::executor::execute, result.value());
+
+    auto res = std::visit(
+        overloaded{
+            [](const drum::commands::NewArgs &arg) {
+              return drum::executor::execute_new(arg);
+            },
+        },
+        result.value());
+
     if (res) {
       std::println("{}", res.value());
       return 1;
